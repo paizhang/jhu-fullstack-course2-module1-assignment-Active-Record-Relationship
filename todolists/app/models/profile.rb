44f7b81701782d8c6186da908bc14ffe03094dee
paizhang <zhangpai91@gmail.com>
2017-06-24 16:15:36 -0700

    Finish all

--- todolists/app/models/profile.rb
+++ todolists/app/models/profile.rb
@@ -1,3 +1,26 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :gender, inclusion: {in: %w(male female)}  # %w(foo bar) is a shortcut for ["foo", "bar"]. Meaning it's a notation to write an array of strings separated by spaces instead of commas and without quotes around them.
+
+  validate :first_name_or_second_name_not_null    # customized validation method should use validate instead of validates
+
+  validate :male_has_first_name_sue
+
+  def first_name_or_second_name_not_null
+    if first_name == nil && last_name == nil
+      errors.add(:first_name, "First name and second name cannot both be null")
+    end
+  end
+
+  def male_has_first_name_sue
+    if gender == "male" && first_name == "Sue"
+      errors.add(:gender, "anyone that is male cannot have the first_name 'Sue'")
+    end
+  end
+
+  def self.get_all_profiles(min_year, max_year)
+    profiles = Profile.where("birth_year BETWEEN ? AND ?", min_year, max_year).order(:birth_year);
+    return profiles;
+  end
 end
--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
-  has_one :profile;
+  has_one :profile, dependent: :destroy;
 
   has_many :todo_lists, dependent: :destroy;    # Using plural names for has_many
 
-  has_many :todo_items, through: :todo_lists, source: :todo_items
+  has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
+
+  validates :username, presence: true
+
+  def get_completed_count
+    count = 0;
+    User.find_by(username: username).todo_items.all.each { |item|
+      if item.completed == true
+        count += 1;
+      end
+    }
+    return count;
+  end
 end
