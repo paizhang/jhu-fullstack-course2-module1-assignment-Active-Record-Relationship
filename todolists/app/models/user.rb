7af7f29dbeaf11bbbc056788e08140815010262a
paizhang <zhangpai91@gmail.com>
2017-06-27 23:11:54 -0700

    Finish module 4 assignment step 5

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
   has_one :profile, dependent: :destroy;
 
   has_many :todo_lists, dependent: :destroy;    # Using plural names for has_many
